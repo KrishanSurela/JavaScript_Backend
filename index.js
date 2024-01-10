b42288d6b259e09f833c927bfa8b89a40dbc31ef
KrishanSurela <surelakrishan60@gmail.com>
2024-01-10 21:01:24 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+const port = 5000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World ! I am Krishan Surela");
+});
+
+app.get("/login", (req, res) => {
+  res.send("DashBoard Page");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`server is listening at ${port}`);
+});
+
+//npm init
+//npm install express
+//at package.json we change "scripts": {
+//    "start": "node index.js"
+//}
+
+//npm run start for run the express code
+
+//npm i dotenv
+
+//create new file name as .env and write the code there
+
+//for add dotenv package to this page =>
+
+// require('dotenv').config()
+
+//for use this package =>
+
+//process.env.variable_name
